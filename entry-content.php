bc9adbbdfc6645d16c5a80984aa77fc683c181d8
David Shanske <david@shanske.com>
2014-06-25 10:47:47 -0400

    Support for Indieweb-Taxonomy Plugin

--- entry-content.php
+++ entry-content.php
@@ -1,11 +1,11 @@
 <?php
         if (!is_search() ) { // Do not display full content on search 
-        $content = "";
-        $content .='<div class="entry-content e-content">';
-        $content .= the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'mf2_bootstrap' ) );
-	$content .= '</div><!-- .entry-content -->';
+        $entrycontent = "";
+        $entrycontent .='<div class="entry-content e-content">';
+        $entrycontent .= get_the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'mf2_bootstrap' ) );
+	$entrycontent .= '</div><!-- .entry-content -->';
 	// entry_content allows data to be inserted above/below the content
-	echo apply_filters ('entry_content', $content);
+	echo apply_filters ('entry_content', $entrycontent);
 	// Pagination outside the content div
                         wp_link_pages( array(
                                 'before' => '<div class="page-links">' . __( 'Pages:', 'mf2_bootstrap' ),
--- inc/plugins.php
+++ inc/plugins.php
@@ -3,9 +3,15 @@
 // Custom Functions for Installed Plugins
 
 // If the Indieweb Taxonomy Plugin is installed, add its display function to the theme
-if(function_exists('content_response_top')){
-     add_filter( 'above_content', 'content_response_top', 20 );
-   }
+if (function_exists('get_response_display')) :
+function indieweb_taxonomy_display($content)
+    {
+        $c = get_response_display() . $content;
+        return $c;
+    }
+ add_filter( 'entry_content', 'indieweb_taxonomy_display', 20 );
+
+endif;
 
 
 ?>
