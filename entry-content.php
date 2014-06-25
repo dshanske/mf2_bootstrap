9e9098bdd7a1333d5af80b4f8f5bba76bbc09ae9
David Shanske <david@shanske.com>
2014-06-25 10:13:31 -0400

    Oops

--- entry-content.php
+++ entry-content.php
@@ -4,7 +4,7 @@
         $content .= the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'mf2_bootstrap' ) );
 	$content .= '</div><!-- .entry-content -->';
 	// entry_content allows data to be inserted above/below the content
-	echo apply_filters ('entry_content', $content)
+	echo apply_filters ('entry_content', $content);
 	// Pagination outside the content div
                         wp_link_pages( array(
                                 'before' => '<div class="page-links">' . __( 'Pages:', 'mf2_bootstrap' ),
